434181a14288f4a42ce34a1a59cc06f469d2b4b0
Sanghoon Han <54487878+hsh200315@users.noreply.github.com>
2023-09-20 23:19:39 +0900

    Delete arrange.sh

--- arrange.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-fileList=($(ls ./files))
-for i in "${!fileList[@]}"; do
-    firstLetter=$(echo ${fileList[i]:0:1} | tr '[:upper:]' '[:lower:]')
-    mv "./files/${fileList[i]}" "./${firstLetter}/"
-done
